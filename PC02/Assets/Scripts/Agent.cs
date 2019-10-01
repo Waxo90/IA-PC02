425a3553bd34b07a3a24f080bafe348c5f4fe7cd
maxCalderon <maxvalentino@hotmail.com>
2019-09-30 21:36:10 -0500

    help

---
+++ PC02/Assets/Scripts/Agent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Agent : SBAgent
+{
+	public Transform target;	
+
+	void Start()
+	{
+		maxSpeed = 10f;
+		maxSteer = 0.5f;		
+	}
+
+	void Update()
+	{
+		velocity += SB.Arrive(this, target, 3);
+		velocity += SB.Flee(this, target, 1);
+		velocity += SB.Separate(this, GameManager.agents, 2f);
+		transform.position += velocity * Time.deltaTime;
+
+        if (transform.position.y < -2 || transform.position.y > 2) {
+            velocity.y *= 0;
+        }
+
+        if (transform.position.x < -2 || transform.position.x > 2) {
+            velocity.x *= 0;
+        }
+	}
+}
---
+++ PC02/Assets/Scripts/Agent2.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Agent2 : SBAgent
+{
+    private int Radio = 2;
+	public Transform target;	
+
+	void Start()
+	{
+		maxSpeed = 10f;
+		maxSteer = 0.5f;		
+	}
+
+	void Update()
+	{
+		velocity += SB.Arrive(this, target, 3);
+		velocity += SB.Flee(this, target, 1);
+		velocity += SB.Separate(this, GameManager.agents, 2f);
+		transform.position += velocity * Time.deltaTime;
+
+       /*  for (int i = 0; i < target.Length; i++) {
+			float angle = 360 * i ;
+			float posX = transform.position.x + Mathf.Sin (angle * Mathf.Deg2Rad) * Radio;
+			float posY = transform.position.y + Mathf.Cos (angle * Mathf.Deg2Rad) * Radio;
+			target [i].position = new Vector3 (posX, posY);
+       }*/
+	}
+}
